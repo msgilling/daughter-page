f6ab4d30b26c1af591d523e586cb536db513e974
msgilling <mxrlling@gmail.com>
2023-06-28 09:19:33 +0100

    updating band img

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './style.css'
-import daughterBand from '../../assets/daughter-new-album.jpg'
+// import daughterBand from '../../assets/daughter-new-album.jpg'
 
 
 const AboutPage = () => {
@@ -12,7 +12,8 @@ const AboutPage = () => {
         <h1>Daughter</h1>
         <p><strong>Genre:</strong> Indie Folk</p>
         <p>Daughter is an English indie folk trio. Fronted by North London native Elena Tonra, the band was formed in 2010 after the addition of Swiss guitarist Igor Haefeli and drummer Remi Aguilella from France. They have released four EPs and three albums, and are currently signed to Glassnote and 4AD.</p>
-        <img src={daughterBand} id="img-band" alt="Image of the band members in Daughter"  height="500px"/>
+        <img src="https://beardedgentlemenmusic.com/wp-content/uploads/2016/02/Daughter-New-Album.jpg" id="img-band" alt="Image of the band members in Daughter"  height="500px"/>
+        {/* <img src={daughterBand} id="img-band" alt="Image of the band members in Daughter"  height="500px"/> */}
       </div>
     </>
   )
